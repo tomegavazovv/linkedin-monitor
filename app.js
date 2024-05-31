acadf33ac4ec48b127e01643678e82a7b4b49cbb
tomegavazovv <tomegavazov@gmail.com>
2024-05-31 15:17:41 +0200

    res size

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Body parser, reading data from body into req.body
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use(express.urlencoded({ extended: true }));
 
